cd1476ec414ae9d9860c8df26e90ca657d77dd08
Thomas Hua <thomas.hua@student.uni-tuebingen.de>
2023-02-17 14:51:45 +0100

    changed image size

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-var googleFinance = require("google-finance");
-
 console.log("Started...");
-
-googleFinance.companyNews({
-    symbol: "NASDAQ:AAPL",
-}, function (err, news) {
-    console.log(err);
-    console.log(news);
-});
 
 console.log("Finished!");
 
